ef9824494ee3dbae606ee7ff25b17541878bee56
Matthieu Devilliers <devilliers.matthieu@gmail.com>
2021-06-13 01:13:48 +0200

    modif textStyle

--- lib/constants/text_style.dart
+++ lib/constants/text_style.dart
@@ -3,74 +3,54 @@ import 'package:flutter/material.dart';
 const TextStyle bodyText1 = TextStyle(
   fontSize: 16,
   letterSpacing: 1,
+  height: 2,
   fontWeight: FontWeight.w400,
   fontFamily: 'Roboto',
-  fontFamilyFallback: [
-    'Roboto',
-    'Arial',
-    'Helvetica',
-    'sans-serif',
-  ],
+);
+
+const TextStyle headline1 = TextStyle(
+  fontSize: 72,
+  letterSpacing: 0.5,
+  height: 1.5,
+  fontWeight: FontWeight.w500,
+  fontFamily: 'KaushanScript',
 );
 
 const TextStyle headline2 = TextStyle(
   fontSize: 30,
   letterSpacing: 0.5,
+  height: 1.2,
   fontWeight: FontWeight.w500,
   fontFamily: 'Raleway',
-  fontFamilyFallback: [
-    'Raleway',
-    'Rubik',
-    'Arial',
-    'Helvetica',
-    'sans-serif',
-  ],
 );
 
 const TextStyle headline3 = TextStyle(
   fontSize: 24,
   letterSpacing: 0.5,
+  height: 1.4,
   fontWeight: FontWeight.w600,
   fontFamily: 'Raleway',
-  fontFamilyFallback: [
-    'Raleway',
-    'Rubik',
-    'Arial',
-    'Helvetica',
-    'sans-serif',
-  ],
 );
 
 const TextStyle headline5 = TextStyle(
   fontSize: 20,
   letterSpacing: 0.5,
+  height: 1.2,
   fontWeight: FontWeight.w400,
   fontFamily: 'Raleway',
-  fontFamilyFallback: [
-    'Raleway',
-    'Rubik',
-    'Arial',
-    'Helvetica',
-    'sans-serif',
-  ],
 );
 
 const TextStyle headline6 = TextStyle(
   fontSize: 18,
   letterSpacing: 0.5,
+  height: 2,
   fontWeight: FontWeight.bold,
   fontFamily: 'Raleway',
-  fontFamilyFallback: [
-    'Raleway',
-    'Rubik',
-    'Arial',
-    'Helvetica',
-    'sans-serif',
-  ],
 );
 const TextStyle button = TextStyle(
   fontSize: 16,
   letterSpacing: 0.5,
+  height: 2,
   fontWeight: FontWeight.w400,
   fontFamily: 'Raleway',
 );
