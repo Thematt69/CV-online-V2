698ee92a6198d00220f711d13ee51f9d3be3da39
Matthieu Devilliers <devilliers.matthieu@gmail.com>
2021-06-13 01:12:41 +0200

    add colors

--- lib/constants/colors.dart
+++ lib/constants/colors.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-const Color darkDarkerColor = Colors.black;
+const Color blackColor = Colors.black;
 const Color darkColor = Color(0xFF090909);
 const Color darkLightColor = Color(0xFF14131B);
 const Color greyColor = Color(0xFF6F6F6F);
@@ -8,5 +8,6 @@ const Color greyLightColor = Color(0xFFF1F1F1);
 const Color orangeDarkColor = Color(0xFFFF4B36);
 const Color orangeColor = Color(0xFFD24E1A);
 const Color orangeLightColor = Color(0xFFFFEDEB);
-const Color whiteColor = Color(0xFFEEEEEE);
+const Color whiteDarkerColor = Color(0xFFEEEEEE);
+const Color whiteColor = Colors.white;
 const Color redColor = Colors.red;
