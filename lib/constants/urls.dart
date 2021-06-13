71e08d780fdd2b72e40ef4bc2bd5d4ff226cef70
Matthieu Devilliers <devilliers.matthieu@gmail.com>
2021-06-13 19:25:54 +0200

    add urls

---
+++ lib/constants/urls.dart
@@ -0,0 +1,14 @@
+const String urlCV = 'https://matthieudevilliers.fr/CV.pdf';
+const String urlMaps = 'https://goo.gl/maps/ztW1mKDHgxkn43B79';
+const String urlMail = 'mailto:devilliers.matthieu@gmail.com';
+const String urlTel = 'tel:+33645564845';
+const String urlTwitch = 'https://www.twitch.tv/thematt69';
+const String urlYoutube =
+    'https://www.youtube.com/channel/UCaB-terB1_dwTdLYwmLaiXA';
+const String urlIndeed = 'https://my.indeed.com/p/matthieud-6v7vi9f';
+const String urlLinkedin =
+    'https://www.linkedin.com/in/matthieu-devilliers-b6a36b15a/';
+const String urlViadeo = 'https://www.viadeo.com/p/002151hk6rbm6fpv';
+const String urlInstagram = 'https://www.instagram.com/thematt69';
+const String urlXefiAcademy = 'https://carriere.xefi.fr/xefi-academy';
+const String urlXefi = 'https://xefi.fr';
