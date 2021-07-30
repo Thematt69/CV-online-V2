ebcb435b9c3998a6819018b038215f1685bc7921
Matthieu Devilliers <devilliers.matthieu@gmail.com>
2021-07-30 23:09:58 +0200

    responsive CustomAnimatedTextKit

--- lib/widgets/custom_animated_text_kit.dart
+++ lib/widgets/custom_animated_text_kit.dart
@@ -2,33 +2,54 @@ import 'package:animated_text_kit/animated_text_kit.dart';
 import 'package:flutter/material.dart';
 
 class CustomAnimatedTextKit extends StatelessWidget {
-  const CustomAnimatedTextKit({Key? key}) : super(key: key);
+  final String prefix;
+  final TextStyle textStyle;
+  final TextAlign textAlign;
+  final Duration duration;
+
+  const CustomAnimatedTextKit({
+    Key? key,
+    this.prefix = '',
+    required this.textStyle,
+    this.textAlign = TextAlign.center,
+    this.duration = const Duration(milliseconds: 100),
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return AnimatedTextKit(
-      totalRepeatCount: 100,
+      repeatForever: true,
       pause: const Duration(seconds: 1),
       animatedTexts: [
         TypewriterAnimatedText(
-          'Développeur Flutter',
-          speed: const Duration(milliseconds: 100),
+          prefix + 'Développeur Flutter',
+          speed: duration,
+          textStyle: textStyle,
+          textAlign: textAlign,
         ),
         TypewriterAnimatedText(
-          'Joueur amateur',
-          speed: const Duration(milliseconds: 100),
+          prefix + 'Joueur amateur',
+          speed: duration,
+          textStyle: textStyle,
+          textAlign: textAlign,
         ),
         TypewriterAnimatedText(
-          'Développeur Web',
-          speed: const Duration(milliseconds: 100),
+          prefix + 'Développeur Web',
+          speed: duration,
+          textStyle: textStyle,
+          textAlign: textAlign,
         ),
         TypewriterAnimatedText(
-          'Photographe amateur',
-          speed: const Duration(milliseconds: 100),
+          prefix + 'Photographe amateur',
+          speed: duration,
+          textStyle: textStyle,
+          textAlign: textAlign,
         ),
         TypewriterAnimatedText(
-          'Développeur Android',
-          speed: const Duration(milliseconds: 100),
+          prefix + 'Développeur Android',
+          speed: duration,
+          textStyle: textStyle,
+          textAlign: textAlign,
         ),
       ],
     );
