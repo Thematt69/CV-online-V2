f06827f3a68cf56c447e246e24aec1050aa931ae
Matthieu Devilliers <devilliers.matthieu@gmail.com>
2021-08-01 18:37:26 +0200

    add urlTwitter + urlDiscord + urlGithub

--- lib/constants/urls.dart
+++ lib/constants/urls.dart
@@ -8,7 +8,9 @@ const String urlYoutube =
 const String urlIndeed = 'https://my.indeed.com/p/matthieud-6v7vi9f';
 const String urlLinkedin =
     'https://www.linkedin.com/in/matthieu-devilliers-b6a36b15a/';
-const String urlViadeo = 'https://www.viadeo.com/p/002151hk6rbm6fpv';
 const String urlInstagram = 'https://www.instagram.com/thematt69';
+const String urlTwitter = 'https://twitter.com/DevilliersMatt';
+const String urlDiscord = 'https://discord.com/';
+const String urlGithub = 'https://github.com/Thematt69';
 const String urlXefiAcademy = 'https://carriere.xefi.fr/xefi-academy';
 const String urlXefi = 'https://xefi.fr';
