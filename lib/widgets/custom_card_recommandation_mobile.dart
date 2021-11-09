99bd8734a9caab81013e168769fa5b5cd458f7fa
Matthieu Devilliers <devilliers.matthieu@gmail.com>
2021-11-09 21:29:33 +0100

    implement recommandation stream firestore

--- lib/widgets/custom_card_recommandation_mobile.dart
+++ lib/widgets/custom_card_recommandation_mobile.dart
@@ -1,80 +1,141 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cv_online_v2/constants/assets.dart';
 import 'package:cv_online_v2/constants/colors.dart';
-import 'package:cv_online_v2/constants/contents.dart';
 import 'package:cv_online_v2/constants/sizes.dart';
-import 'package:cv_online_v2/localization/localization.dart';
+import 'package:cv_online_v2/models/recommandation.dart';
 import 'package:cv_online_v2/responsive.dart';
 import 'package:flutter/material.dart';
 
-class CustomCardRecommandationMobile extends StatelessWidget {
+class CustomCardRecommandationMobile extends StatefulWidget {
   const CustomCardRecommandationMobile({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<CustomCardRecommandationMobile> createState() =>
+      _CustomCardRecommandationMobileState();
+}
+
+class _CustomCardRecommandationMobileState
+    extends State<CustomCardRecommandationMobile> {
+  final Stream<QuerySnapshot<Map<String, dynamic>>> _recommandationsStream =
+      FirebaseFirestore.instance.collection('recommandations').snapshots();
+
+  @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(defaultPadding30),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Text(
-            translations.text('contents.recommandation'),
-            style: Theme.of(context).textTheme.headline6?.copyWith(
-                  letterSpacing: 0,
-                  height: 1.6,
-                  fontWeight: FontWeight.w300,
-                  color: whiteDarkerColor,
-                ),
-          ),
-          const SizedBox(height: defaultPadding30),
-          Text(
-            auteurRecommandation,
-            style: Theme.of(context).textTheme.headline3?.copyWith(
-                  letterSpacing: 0,
-                  fontWeight: FontWeight.w300,
-                  fontSize: 18,
-                  color: greyColor,
-                ),
-          ),
-          const SizedBox(height: defaultPadding6),
-          Row(
-            children: [
-              Expanded(
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Text(
-                      translations.text('contents.poste_recommandation'),
-                      style: Theme.of(context).textTheme.headline5?.copyWith(
-                            color: orangeColor,
-                            fontSize: 18,
-                          ),
-                    ),
-                    Text(
-                      entrepriseRecommandation,
-                      style: Theme.of(context).textTheme.headline5?.copyWith(
-                            color: orangeColor,
-                            fontSize: 18,
-                          ),
-                    ),
-                  ],
-                ),
-              ),
-              Visibility(
-                visible: Responsive.isTablet(context),
-                child: Image.asset(
-                  assetDoubleQuote,
-                  width: 60,
-                  cacheWidth: 60,
-                  errorBuilder: (context, error, stackTrace) => const SizedBox(
-                    width: 60,
+      child: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+        stream: _recommandationsStream,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            debugPrint(snapshot.error.toString());
+            debugPrintStack(stackTrace: snapshot.stackTrace);
+            return Text(
+              'Erreur lors de la récupération de la recommandation',
+              style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                    color: Theme.of(context).colorScheme.onBackground,
+                    fontWeight: FontWeight.w500,
+                    height: 1,
                   ),
+            );
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Row(
+              children: [
+                Container(
+                  width: 20,
+                  height: 20,
+                  margin: const EdgeInsets.all(4),
+                  child: const CircularProgressIndicator(),
                 ),
+                const SizedBox(width: 4),
+                Text(
+                  'Récupération de la recommandation en cours...',
+                  style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                        color: Theme.of(context).colorScheme.onBackground,
+                        fontWeight: FontWeight.w500,
+                        height: 1,
+                      ),
+                ),
+              ],
+            );
+          }
+
+          final List<Recommandation> listRecommandations = snapshot.data!.docs
+              .map(
+                (document) => Recommandation.fromFireStore(document.data()),
+              )
+              .toList();
+
+          final Recommandation recommandation = listRecommandations.first;
+
+          return Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                recommandation.texte.currentLang,
+                style: Theme.of(context).textTheme.headline6?.copyWith(
+                      letterSpacing: 0,
+                      height: 1.6,
+                      fontWeight: FontWeight.w300,
+                      color: whiteDarkerColor,
+                    ),
               ),
+              const SizedBox(height: defaultPadding30),
+              Text(
+                recommandation.auteur.currentLang,
+                style: Theme.of(context).textTheme.headline3?.copyWith(
+                      letterSpacing: 0,
+                      fontWeight: FontWeight.w300,
+                      fontSize: 18,
+                      color: greyColor,
+                    ),
+              ),
+              const SizedBox(height: defaultPadding6),
+              Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          recommandation.poste.currentLang,
+                          style:
+                              Theme.of(context).textTheme.headline5?.copyWith(
+                                    color: orangeColor,
+                                    fontSize: 18,
+                                  ),
+                        ),
+                        Text(
+                          recommandation.entreprise.currentLang,
+                          style:
+                              Theme.of(context).textTheme.headline5?.copyWith(
+                                    color: orangeColor,
+                                    fontSize: 18,
+                                  ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Visibility(
+                    visible: Responsive.isTablet(context),
+                    child: Image.asset(
+                      assetDoubleQuote,
+                      width: 60,
+                      cacheWidth: 60,
+                      errorBuilder: (context, error, stackTrace) =>
+                          const SizedBox(
+                        width: 60,
+                      ),
+                    ),
+                  ),
+                ],
+              )
             ],
-          )
-        ],
+          );
+        },
       ),
     );
   }
--- lib/widgets/custom_card_recommandation_web.dart
+++ lib/widgets/custom_card_recommandation_web.dart
@@ -1,14 +1,24 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:cv_online_v2/constants/assets.dart';
 import 'package:cv_online_v2/constants/colors.dart';
-import 'package:cv_online_v2/constants/contents.dart';
 import 'package:cv_online_v2/constants/sizes.dart';
-import 'package:cv_online_v2/localization/localization.dart';
+import 'package:cv_online_v2/models/recommandation.dart';
 import 'package:flutter/material.dart';
 
-class CustomCardRecommandationWeb extends StatelessWidget {
+class CustomCardRecommandationWeb extends StatefulWidget {
   const CustomCardRecommandationWeb({
     Key? key,
   }) : super(key: key);
+
+  @override
+  State<CustomCardRecommandationWeb> createState() =>
+      _CustomCardRecommandationWebState();
+}
+
+class _CustomCardRecommandationWebState
+    extends State<CustomCardRecommandationWeb> {
+  final Stream<QuerySnapshot<Map<String, dynamic>>> _recommandationsStream =
+      FirebaseFirestore.instance.collection('recommandations').snapshots();
 
   @override
   Widget build(BuildContext context) {
@@ -32,44 +42,93 @@ class CustomCardRecommandationWeb extends StatelessWidget {
           ),
           const SizedBox(width: defaultPadding30),
           Expanded(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  translations.text('contents.recommandation'),
-                  style: Theme.of(context).textTheme.headline6?.copyWith(
-                        letterSpacing: 0,
-                        height: 1.6,
-                        fontWeight: FontWeight.w300,
-                        color: whiteDarkerColor,
+            child: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+              stream: _recommandationsStream,
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  debugPrint(snapshot.error.toString());
+                  debugPrintStack(stackTrace: snapshot.stackTrace);
+                  return Text(
+                    'Erreur lors de la récupération de la recommandation',
+                    style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                          color: Theme.of(context).colorScheme.onBackground,
+                          fontWeight: FontWeight.w500,
+                          height: 1,
+                        ),
+                  );
+                }
+
+                if (snapshot.connectionState == ConnectionState.waiting) {
+                  return Row(
+                    children: [
+                      Container(
+                        width: 20,
+                        height: 20,
+                        margin: const EdgeInsets.all(4),
+                        child: const CircularProgressIndicator(),
                       ),
-                ),
-                const SizedBox(height: defaultPadding30),
-                Text(
-                  auteurRecommandation,
-                  style: Theme.of(context).textTheme.headline3?.copyWith(
-                        letterSpacing: 0,
-                        fontWeight: FontWeight.w300,
-                        fontSize: 18,
-                        color: greyColor,
+                      const SizedBox(width: 4),
+                      Text(
+                        'Récupération de la recommandation en cours...',
+                        style: Theme.of(context).textTheme.bodyText1!.copyWith(
+                              color: Theme.of(context).colorScheme.onBackground,
+                              fontWeight: FontWeight.w500,
+                              height: 1,
+                            ),
                       ),
-                ),
-                const SizedBox(height: defaultPadding6),
-                Text(
-                  translations.text('contents.poste_recommandation'),
-                  style: Theme.of(context).textTheme.headline5?.copyWith(
-                        color: orangeColor,
-                        fontSize: 18,
-                      ),
-                ),
-                Text(
-                  entrepriseRecommandation,
-                  style: Theme.of(context).textTheme.headline5?.copyWith(
-                        color: orangeColor,
-                        fontSize: 18,
-                      ),
-                ),
-              ],
+                    ],
+                  );
+                }
+
+                final List<Recommandation> listRecommandations =
+                    snapshot.data!.docs
+                        .map(
+                          (document) =>
+                              Recommandation.fromFireStore(document.data()),
+                        )
+                        .toList();
+
+                final Recommandation recommandation = listRecommandations.first;
+                return Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      recommandation.texte.currentLang,
+                      style: Theme.of(context).textTheme.headline6?.copyWith(
+                            letterSpacing: 0,
+                            height: 1.6,
+                            fontWeight: FontWeight.w300,
+                            color: whiteDarkerColor,
+                          ),
+                    ),
+                    const SizedBox(height: defaultPadding30),
+                    Text(
+                      recommandation.auteur.currentLang,
+                      style: Theme.of(context).textTheme.headline3?.copyWith(
+                            letterSpacing: 0,
+                            fontWeight: FontWeight.w300,
+                            fontSize: 18,
+                            color: greyColor,
+                          ),
+                    ),
+                    const SizedBox(height: defaultPadding6),
+                    Text(
+                      recommandation.poste.currentLang,
+                      style: Theme.of(context).textTheme.headline5?.copyWith(
+                            color: orangeColor,
+                            fontSize: 18,
+                          ),
+                    ),
+                    Text(
+                      recommandation.entreprise.currentLang,
+                      style: Theme.of(context).textTheme.headline5?.copyWith(
+                            color: orangeColor,
+                            fontSize: 18,
+                          ),
+                    ),
+                  ],
+                );
+              },
             ),
           ),
         ],
